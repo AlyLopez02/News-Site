78bfbd9485c25c95034e11f11e753365fe899774
AlyLopez02 <alylopez0217@gmail.com>
2022-10-20 21:48:52 -0400

    stub out img and nav components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      header
+      <header>
+        <img src="https://via.placeholder.com/214x64" alt="placeholder" />
+        <nav>
+          <ul>
+            <li>Workspace</li>
+            <li>Membership</li>
+            <li>Community</li>
+          </ul>
+
+          <ul>
+            <li>Contact</li>
+            <li>Join us</li>
+            <li>Login</li>
+          </ul>
+
+        </nav>
+      </header>
     </div>
   );
 }
