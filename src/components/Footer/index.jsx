7da709c2a35f2d53d82afe64d2d2a2a79faef74c
AlyLopez02 <alylopez0217@gmail.com>
2022-10-23 13:49:20 -0400

    stub out second container

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import './style.scss';
 
+const mainLinks = [
+  'Link 1',
+  'Link2',
+  'Link3',
+];
+
+const subLinks = [
+  'Sub link 1',
+  'Sub link 2',
+  'Sub link 3',
+  'Sub link 4',
+  'Sub link 5',
+];
+
+const lastLinks = [
+  'Terms & Conditions',
+  ' | ',
+  'Privacy Policy',
+  ' | ',
+  'FAQs',
+];
+
 function Footer() {
   return (
-    <div>
-      hello
+    <div className="footer">
+      <div id="container1">
+        <nav>links</nav>
+        <div>newsletter and socials</div>
+      </div>
+      <div className="container2">
+        <p>@2021 CompanyName. All right reserved</p>
+        <ul>
+          {lastLinks.map((link) => (
+            <li>{link}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
